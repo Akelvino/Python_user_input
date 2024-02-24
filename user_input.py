1ccb2d741a6ab9159c548b44e2ed89f64b504df9
Akelvino <vinho892@gmail.com>
2024-02-24 08:19:32 +0300

    I have finished my project

--- user_input.py
+++ user_input.py
@@ -1,1 +1,5 @@
-print("Hello World!!")
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+location = input("where do you currently reside?\n")
+
+print('my name is {}. I am {} years old. I reside in {}'.format(name, age, location))
